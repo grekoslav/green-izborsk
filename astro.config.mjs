1347b4b8818f415bbca809faf2ff652e76227f1b
grekoslav <grekoslav@gmail.com>
2026-07-31 16:29:16 +0300

    Настройка автодеплоя на хостинг Timeweb

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://grekoslav.github.io',
-  base: '/green-izborsk',
+  site: 'https://green-izborsk.ru',
 });
+
